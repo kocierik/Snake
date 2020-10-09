544a491dd87932fd83216c03da4653c1015407ae
Erik Koci <kocierik@gmail.com>
2020-10-09 13:42:42 +0200

    feat: add image background

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let bg
 function setup() {
     divPoints = createDiv().addClass('points');
     createCanvas(400, 400)
-        //bg = loadImage('./assets/background.jpg');
+    bg = loadImage("assets/background.jpg");
     frameRate(10)
     player = new Snake(20, 20, 20, 0, fill(color('gray')));
     point = new Point(100, 100, fill(color(255, 0, 0)))
@@ -20,7 +20,7 @@ function pointGame() {
 
 function draw() {
     pointGame()
-    background(41)
+    background(bg)
     player.createSnake()
     player.update()
     player.show()
