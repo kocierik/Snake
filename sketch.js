85bca4b9ccc0ef0022b4931bd6206cbb833f7136
Erik Koci <kocierik@gmail.com>
2020-10-09 13:07:45 +0200

    feat: add points

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
 let player
 let point
 let tail = []
+let divPoints
+let bg
 
 function setup() {
+    divPoints = createDiv().addClass('points');
     createCanvas(400, 400)
-    background(41)
+        //bg = loadImage('./assets/background.jpg');
     frameRate(10)
     player = new Snake(20, 20, 20, 0, fill(color('gray')));
     point = new Point(100, 100, fill(color(255, 0, 0)))
 
 }
 
+function pointGame() {
+    divPoints.innerHTML = divPoints.html('score: ' + player.score, false);
+}
+
 function draw() {
+    pointGame()
     background(41)
     player.createSnake()
     player.update()
